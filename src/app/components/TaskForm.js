e3d7b478fdb691de2c172bfb68337c68935786f3
zaludja <jakub.zalud.0804@gmail.com>
2026-06-03 12:19:41 +0200

    barvičky

--- src/app/components/TaskForm.js
+++ src/app/components/TaskForm.js
@@ -5,7 +5,6 @@ import { zodResolver } from '@hookform/resolvers/zod'
 import { z } from 'zod'
 import { useRouter } from 'next/navigation'
 import DatePicker from 'react-datepicker'
-import 'react-datepicker/dist/react-datepicker.css'
 import { supabase } from '@/utils/supabase'
 
 const schema = z.object({
